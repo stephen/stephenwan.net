1c24c0d30958f27e58786721893baad830455264
Stephen Wan <stephen@stephenwan.net>
2014-09-07 02:10:49 -0500

    Updates to how page is rendered

--- js/main.js
+++ js/main.js
@@ -1,96 +1,44 @@
-function isTouchDevice() {
-   return (('ontouchstart' in window) || (navigator.MaxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0));
-}
+var heroSlides = [
+  { bg: '/static/sand.jpg', action: 'constructs &amp; destructs' },
+  { bg: '/static/night.jpg', action: 'is the night' },
+  { bg: '/static/sunset.jpg', action: 'watches sunsets' },
+  { bg: '/static/batman.jpg', action: 'fights crime' },
+  { bg: '/static/chair.jpg', action: 'sits in chairs' },
+  { bg: '/static/hacktx.jpg', action: 'hacks texas' },
+  { bg: '/static/duck.jpg', action: 'quacks (sometimes)' },
+  { bg: '/static/hario.jpg', action: 'makes coffee' },
+  { bg: '/static/bed.jpg', action: 'sleeps a lot' },
+  { bg: '/static/michelle.jpg', action: 'has possessions' },
+  { bg: '/static/library.jpg', action: 'owns books' }
+];
 
-var FrontpageManager = function() {
-  var self = this;
-  self.map = [
-    { bg: '/static/sand.jpg', action: 'constructs &amp; destructs', center: '50% 20%' },
-    { bg: '/static/night.jpg', action: 'is the night', center: '50% 50%' },
-    { bg: '/static/boat.jpg', action: 'is on a boat', center: '50% 50%' },
-    { bg: '/static/sunset.jpg', action: 'watches sunsets', center: '50% 50%' },
-    { bg: '/static/batman.jpg', action: 'fights crime', center: '50% 50%' },
-    { bg: '/static/chair.jpg', action: 'sits in chairs', center: '50% 50%' },
-    { bg: '/static/hacktx.jpg', action: 'hacks texas', center: '50% 50%' },
-    { bg: '/static/duck.jpg', action: 'quacks (sometimes)', center: '50% 50%' },
-    { bg: '/static/hario.jpg', action: 'makes coffee', center: '50% 50%' },
-    { bg: '/static/bed.jpg', action: 'sleeps much', center: '50% 50%' },
-    { bg: '/static/michelle.jpg', action: 'enjoys beauty', center: '50% 50%' },
-    { bg: '/static/library.jpg', action: 'owns books', center: '50% 20%' },
-  ];
-  self.last = { bg: '/static/sparks.jpg', action: 'would like to meet you', center: '50% 50%' };
-};
+heroSlides.push({ bg: '/static/sparks.jpg', action: 'would like to meet you' });
 
-FrontpageManager.prototype.show = function(index) {
-  var item = this.map[index];
-  if (this.map.length == 0) {
-    item = this.last;
-    $('#frontpage .action').unbind('click').click(function() {
-      window.location = 'mailto:stephen@stephenwan.net';
-    });
-  }
-
-  this.currentImg = item.bg;
-  $('#frontpage').css('background-image', "url('" + item.bg + "')");
-  $('#frontpage .action').html(item.action);
-  this.map.splice(index, 1);
-};
-
-FrontpageManager.prototype.showRandom = function() {
-  this.show(Math.floor(Math.random() * this.map.length));
-};
-
-$(document).ready(function() {
-
-  var fpMan = new FrontpageManager();
-  fpMan.showRandom();
-
-  var mainBackground = new Image();
-  $(mainBackground).load(function() {
-    $('.page#loading').css('opacity', 0);
-    $('.heading').addClass('animate-in');
-    
-    if (isTouchDevice()) {
-      // on mobile, show after small delay
-      setTimeout(function() {
-        $('footer').addClass('animate-in');
-      }, 500);
-
-      $('.scene').parallax({
-        calibrateX: false,
-        calibrateY: true
-      });
-
-    } else {
-      // on non-mobile, show when mouse moves
-      $('body').one('mousemove', function() {
-        setTimeout(function() {
-          $('footer').addClass('animate-in');
-        }, 500);
-      });
-
-    }
-
-    // preload the rest!
-    fpMan.map.forEach(function(element) {
-      $('<img/>')[0].src = element.bg;
-    });
-
-    $('<img/>')[0].src = fpMan.last.bg;
-
+// helpers
+var template = _.template($('#pane-background-template').text());
+var displaySlide = function(slide) {
+  var content = template({
+    src: slide.bg
   });
 
-  // preload first image
-  mainBackground.src = fpMan.currentImg;
+  $('.pane#background').append(content);
+};
 
-  $('.tagline').click(function() {
-    $('#frontpage .action').fadeIn(100);
-    $(this).fadeOut(100);
-    return false;
-  });
+// choose slide + show
+var slide = _.sample(heroSlides);
+displaySlide(slide);
 
-  $('#frontpage a.action').click(function() {
-    fpMan.showRandom();
-  });
+var preloader = new Image();
+preloader.src = slide.bg;
 
+// start animations on load
+$(preloader).on('load', function() {
+  $('.pane#background div:first,footer,header,.pane#reveal').addClass('animate-in');
 });
+
+
+
+
+
+
+
